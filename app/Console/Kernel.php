4df5444af426941c8c467294187cbd5ce14efd63
dhv0612 <dhv0612@gmail.com>
2022-03-14 12:50:35 +0700

    update daily check with schedule everyday

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,17 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        'App\Console\Commands\DailyUserInactive',
+        'App\Console\Commands\DailyPostUnread'
+    ];
+
     /**
      * Define the application's command schedule.
      *
@@ -15,7 +26,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+         $schedule->command('daily:user_inactive')->daily();
+         $schedule->command('daily:post_unread')->daily();
     }
 
     /**
